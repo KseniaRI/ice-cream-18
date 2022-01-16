393860a6052bb972bcb091578a5c120d35a86bf5
Victoria <92218796+Highgradecode@users.noreply.github.com>
2022-01-17 01:31:44 +0200

    starting the modal-about window

---
+++ modal-about.js
@@ -0,0 +1,17 @@
+
+(() => {
+  const refs = {
+    openAboutModalBtn: document.querySelector(".open-about-modal-btn"),
+    closeAboutModalBtn: document.querySelector(".close-about-modal-btn"),
+    modal: document.querySelector(".about-backdrop"),
+  };
+
+  refs.openAboutModalBtn.addEventListener('click', toggleModal);
+  refs.closeAboutModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
+
+
